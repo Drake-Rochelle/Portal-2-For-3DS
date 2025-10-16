d99cba10aac31c71cbc037a6be57ca786dd062ca
Drake-Rochelle <drakerochelle.3ds@gmail.com>
2025-10-16 16:49:00 -0700

    Added Companion Cube
    Added Companion Cube

---
+++ Assets/EditorScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+public class EditorScript : MonoBehaviour
+{
+#if UNITY_EDITOR
+    void Start()
+    {
+
+    }
+    public virtual void OnUpdate()
+    {
+
+    }
+    void Update()
+    {
+        if (!UnityEditor.EditorApplication.isPlaying)
+        {
+            OnUpdate();
+        }
+    }
+#endif
+}
--- Assets/LightingBakingPrepper.cs
+++ Assets/LightingBakingPrepper.cs
@@ -1,16 +1,86 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-
-public class LightingBakingPrepper : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[ExecuteInEditMode]
+public class LightingBakingPrepper : EditorScript
+{
+#if UNITY_EDITOR
+    [SerializeField] private bool bake;
+    private bool prevBake;
+    private List<GameObject> noShadowCasters;
+    private bool moved;
+    private List<Transform> objTransforms;
+    void Awake()
+    {
+        objTransforms = new List<Transform>();
+    }
+    void Update()
+    {
+        Debug.Log(prevBake);
+        Debug.Log(bake);
+        if (bake && !prevBake)
+        {
+            OnBakeStart();
+        }
+        prevBake = bake;
+        if (!bake && objTransforms.Count!=0)
+        {
+            for (int i = 0; i < noShadowCasters.Count; i++)
+            {
+                noShadowCasters[i].transform.position = objTransforms[i].position;
+            }
+            objTransforms.Clear();
+        }
+    }
+    private void OnBakeStart()
+    {
+        moved = true;
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        List<GameObject> noShadowCasters = new List<GameObject>();
+        Debug.Log(allObjects.Length);
+        foreach (GameObject obj in allObjects)
+        {
+            if (!obj.isStatic)
+            {
+                if (obj.transform.parent == null)
+                {
+                    Debug.Log(obj.name);
+                    noShadowCasters.Add(obj);
+                    objTransforms.Add(obj.transform);
+                }
+                else
+                {
+                    if (obj.transform.parent.gameObject.isStatic)
+                    {
+                        Debug.Log(obj.name);
+                        noShadowCasters.Add(obj);
+                        objTransforms.Add(obj.transform);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < noShadowCasters.Count; i++)
+        {
+            noShadowCasters[i].transform.position = new Vector3(
+                noShadowCasters[i].transform.position.x * 99,
+                noShadowCasters[i].transform.position.y * 99,
+                noShadowCasters[i].transform.position.z * 99
+            );
+        }
+    }
+    private void OnBakeFinish()
+    {
+        if (moved)
+        {
+            moved = false;
+            for (int i = 0; i < noShadowCasters.Count; i++)
+            {
+                noShadowCasters[i].transform.position = new Vector3(
+                    noShadowCasters[i].transform.position.x / 99,
+                    noShadowCasters[i].transform.position.y / 99,
+                    noShadowCasters[i].transform.position.z / 99
+                );
+            }
+        }
+    }
+#endif
 }
